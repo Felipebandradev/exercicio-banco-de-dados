2e04eb62a9e58e3fa87bedd3988a227eb9d70c67
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2023-08-21 11:01:20 -0300

    Resolução da Etapa 2 (CRUD)

---
+++ dump/tecinternet_escola_felipe_barbosa_dados_cadastrados.sql
@@ -0,0 +1,177 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 21-Ago-2023 às 16:00
+-- Versão do servidor: 10.4.28-MariaDB
+-- versão do PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `tecinternet_escola_felipe_barbosa`
+--
+CREATE DATABASE IF NOT EXISTS `tecinternet_escola_felipe_barbosa` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `tecinternet_escola_felipe_barbosa`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `alunos`
+--
+
+CREATE TABLE `alunos` (
+  `id` tinyint(4) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `data_de_nascimento` date NOT NULL,
+  `primeira_nota` decimal(4,2) NOT NULL,
+  `segunda_nota` decimal(4,2) NOT NULL,
+  `curso_id` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `alunos`
+--
+
+INSERT INTO `alunos` (`id`, `nome`, `data_de_nascimento`, `primeira_nota`, `segunda_nota`, `curso_id`) VALUES
+(1, 'Harley Quinn', '1992-08-10', 10.00, 7.50, 5),
+(2, 'Melissa', '1999-03-04', 3.50, 2.10, 2),
+(3, 'Elielison', '1990-03-12', 1.50, 8.00, 4),
+(4, 'Robôcop', '2000-02-15', 7.70, 8.80, 4),
+(5, 'Renata', '1955-08-21', 8.60, 9.50, 3),
+(6, 'Seraphine', '2007-09-05', 10.00, 5.00, 5),
+(7, 'Zerine', '1998-01-17', 4.50, 3.00, 3),
+(8, 'Katarina', '1968-10-09', 9.50, 8.70, 2),
+(9, 'Alineane', '1988-12-28', 9.90, 8.50, 1),
+(10, 'Josefina', '1973-11-18', 2.00, 1.50, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cursos`
+--
+
+CREATE TABLE `cursos` (
+  `id` tinyint(4) NOT NULL,
+  `titulo` varchar(30) NOT NULL,
+  `carga_horaria` tinyint(4) NOT NULL,
+  `professor_id` tinyint(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `cursos`
+--
+
+INSERT INTO `cursos` (`id`, `titulo`, `carga_horaria`, `professor_id`) VALUES
+(1, 'Front-End', 40, 5),
+(2, 'Back-End', 80, 4),
+(3, 'UX/UI', 30, 3),
+(4, 'Figma', 10, 2),
+(5, 'Rede de Computadores', 100, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `professores`
+--
+
+CREATE TABLE `professores` (
+  `id` tinyint(4) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `area_de_atuacao` enum('Desing','Desenvolvimento','Infra') DEFAULT NULL,
+  `curso_id` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `professores`
+--
+
+INSERT INTO `professores` (`id`, `nome`, `area_de_atuacao`, `curso_id`) VALUES
+(1, 'Jon Oliva', 'Infra', 5),
+(2, 'Lemmy Kilmister', 'Desing', 4),
+(3, 'Neil Peart', 'Desing', 3),
+(4, ' Ozzy Osbourne', 'Desenvolvimento', 2),
+(5, 'David Gilmour', 'Desenvolvimento', 1);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `alunos`
+--
+ALTER TABLE `alunos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_alunos_cursos` (`curso_id`);
+
+--
+-- Índices para tabela `cursos`
+--
+ALTER TABLE `cursos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_cursos_professores1` (`professor_id`);
+
+--
+-- Índices para tabela `professores`
+--
+ALTER TABLE `professores`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_professores_cursos1` (`curso_id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `alunos`
+--
+ALTER TABLE `alunos`
+  MODIFY `id` tinyint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de tabela `cursos`
+--
+ALTER TABLE `cursos`
+  MODIFY `id` tinyint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de tabela `professores`
+--
+ALTER TABLE `professores`
+  MODIFY `id` tinyint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `alunos`
+--
+ALTER TABLE `alunos`
+  ADD CONSTRAINT `fk_alunos_cursos` FOREIGN KEY (`curso_id`) REFERENCES `cursos` (`id`);
+
+--
+-- Limitadores para a tabela `cursos`
+--
+ALTER TABLE `cursos`
+  ADD CONSTRAINT `fk_cursos_professores1` FOREIGN KEY (`professor_id`) REFERENCES `professores` (`id`);
+
+--
+-- Limitadores para a tabela `professores`
+--
+ALTER TABLE `professores`
+  ADD CONSTRAINT `fk_professores_cursos1` FOREIGN KEY (`curso_id`) REFERENCES `cursos` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ resolucao_etapa2.md
@@ -0,0 +1,108 @@
+# Resolução da Etapa 2 (CRUD) 
+
+# Cadastro-Geral
+
+## CREATE
+
+###  Cursos
+
+- código
+```sql
+INSERT INTO cursos(
+    titulo, carga_horaria  
+) 
+VALUES (
+    "Front-End",  40
+),(
+    "Back-End", 80
+),(
+    "UX/UI",  30
+),(
+    "Figma", 10
+),(
+    "Rede de Computadores", 100
+);
+```
+
+- resultado visual
+![Tabela com os cursos já cadastrados](/img/dados-tabela-cursos.png)
+
+### Professores
+
+- código
+```sql
+INSERT INTO professores (
+    nome, area_de_atuacao, curso_id  
+) 
+VALUES (
+    "Jon Oliva",  'Infra', 5
+),(
+    "Lemmy Kilmister", 'Desing', 4
+),(
+    "Neil Peart", 'Desing', 3
+),(
+    " Ozzy Osbourne", 'Desenvolvimento',2
+),(
+    "David Gilmour", 'Desenvolvimento',1
+);
+```
+- resultado visual
+![Tabela com os professores já cadastrados](/img/dados-tabela-professores.png)
+
+### Alunos
+
+- código
+```sql
+INSERT INTO alunos (
+    nome, data_de_nascimento, primeira_nota, segunda_nota, curso_id  
+) 
+VALUES (
+    "Harley Quinn", '1992-08-10', 10.00, 7.5, 5
+),(
+    "Melissa", '1999-03-04', 3.5, 2.1, 2
+),(
+    "Elielison", '1990-03-12', 1.5, 8.0, 4
+),(
+    "Robôcop", '2000-02-15', 7.7, 8.8, 4
+),(
+    "Renata", '1955-08-21',8.6, 9.5, 3
+),(
+    "Seraphine", '2007-09-05', 10.00, 5.0, 5
+),(
+    "Zerine", '1998-01-17', 4.5, 3.0, 3
+),(
+    "Katarina", '1968-10-09', 9.5, 8.7, 2
+),(
+    "Alineane", '1988-12-28', 9.9, 8.5, 1
+),(
+    "Josefina",'1973-11-18', 2.0, 1.5, 1
+);
+```
+
+- resultado visual
+![Tabela com alunos ja cadastrados](/img/dados-tabela-alunos.png)
+
+## UPDATE
+
+### Colocando os Professores nos Cursos
+
+- código
+```sql
+-- Front-End
+UPDATE cursos SET professor_id = 5 WHERE id = 1;
+
+-- Back-End
+UPDATE cursos SET professor_id = 4 WHERE id = 2;
+
+-- UX/UI
+UPDATE cursos SET professor_id = 3 WHERE id = 3;
+
+-- Figma
+UPDATE cursos SET professor_id = 2 WHERE id = 4;
+
+-- Rede de computadores
+UPDATE cursos SET professor_id = 1 WHERE id = 5;
+```
+
+- Resultado visual <br>
+![Professor id Cadastrado](/img/professores_id_cadastrado.png)
